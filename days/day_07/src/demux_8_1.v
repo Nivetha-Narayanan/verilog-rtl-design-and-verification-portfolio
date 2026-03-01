a439a0a7ebb0a7fd41d4cde6ddb472f30ada7010
Nivetha-Narayanan <nivethanarayanan6@gmail.com>
2026-03-01 19:39:19 +0530

    Update demux_8_1.v

--- days/day_07/src/demux_8_1.v
+++ days/day_07/src/demux_8_1.v
@@ -1,1 +1,7 @@
+module demux_1_8(input i,input [2:0] S,output [7:0] y);
+wire w1,w2; 
 
+demux_1_2 m1(.i(i),.sel(S[2]),.y({w2,w1}));
+demux_1_4 m2(.i(w1),.sel(S[1:0]),.y(y[3:0]));
+demux_1_4 m3(.i(w2),.sel(S[1:0]),.y(y[7:4]));
+endmodule
