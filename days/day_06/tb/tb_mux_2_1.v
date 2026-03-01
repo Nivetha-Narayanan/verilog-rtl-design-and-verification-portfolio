937476974a252b5521c2c2320c70c27ad344a3fd
Nivetha-Narayanan <nivethanarayanan6@gmail.com>
2026-03-01 13:45:53 +0530

    Add testbench for 2-to-1 multiplexer

--- days/day_06/tb/tb_mux_2_1.v
+++ days/day_06/tb/tb_mux_2_1.v
@@ -1,1 +1,16 @@
+`timescale 1ns / 1ps
+module tb_mux_2_1();
+reg s,i0,i1;
+wire out;
+mux_2_1 uut(s,i0,i1,out);
+initial begin
+$display("Sel|  i0 | i1 | out ");
+$monitor("%b |  %b  |  %b | %b ",s,i0,i1,out);
+s=0;i0=0;i1=1;#10;
+s=1; #10;
+$finish;
+end
+
+endmodule
+
 
