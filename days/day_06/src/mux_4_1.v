41d12095d345580cfd1959d99c97a14411e5bddb
Nivetha-Narayanan <nivethanarayanan6@gmail.com>
2026-03-01 14:13:32 +0530

    Update mux_4_1.v

--- days/day_06/src/mux_4_1.v
+++ days/day_06/src/mux_4_1.v
@@ -1,1 +1,11 @@
+module mux_4_1(input [1:0] S,input i0,i1,i2,i3,output reg out);
+always @(*) begin
+case(S)
+2'b00: out=i0;
+2'b01: out=i1;
+2'b10: out=i2;
+2'b11: out=i3;
+endcase
 
+end
+endmodule
