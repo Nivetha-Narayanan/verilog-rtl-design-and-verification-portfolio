d9a73bb37b49ec407dc6ab4a8be21e918717fb25
Nivetha-Narayanan <nivethanarayanan6@gmail.com>
2026-03-01 19:38:51 +0530

    Add 1-to-4 demultiplexer module

--- days/day_07/src/demux_4_1.v
+++ days/day_07/src/demux_4_1.v
@@ -1,1 +1,6 @@
-
+module demux_1_4(input i,input [1:0] sel,output [3:0] y);
+assign y[0]=i&(~sel[1]&~sel[0]);
+assign y[1]=i&(~sel[1]&sel[0]);
+assign y[2]=i&(sel[1]&~sel[0]);
+assign y[3]=i&(sel[1]&sel[0]);
+endmodule
