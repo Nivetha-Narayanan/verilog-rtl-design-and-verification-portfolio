e761a69e49bc333624dad23c328d7ee3d375beff
Nivetha-Narayanan <nivethanarayanan6@gmail.com>
2026-03-01 19:37:25 +0530

    Add testbench for demux_1_2 module

--- days/day_07/tb/tb_demux_2_1.v
+++ days/day_07/tb/tb_demux_2_1.v
@@ -1,1 +1,18 @@
+`timescale 1ns / 1ps
+module tb_demux_1_2();
+reg i,sel;
+wire [1:0] y;
+demux_1_2 uut(i,sel,y);
+initial begin
+$display("i  |sel  | y1 y0");
+$monitor("%b | %b  | %b",i,sel,y);
+i=0;
+sel=0;#10;
+sel=1;#10;
+i=1;
+sel=0;#10;
+sel=1;#10;
+$finish;
+end
+endmodule
 
