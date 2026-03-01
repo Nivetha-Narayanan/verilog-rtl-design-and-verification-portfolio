4004a36ac0752fb1b823cb632e2213dc2e1d51fa
Nivetha-Narayanan <nivethanarayanan6@gmail.com>
2026-03-01 19:38:26 +0530

    Add testbench for 1-to-8 demultiplexer

--- days/day_07/tb/tb_demux_8_1.v
+++ days/day_07/tb/tb_demux_8_1.v
@@ -1,1 +1,22 @@
+`timescale 1ns / 1ps
+module tb_demux_1_8();
+reg i;
+reg [2:0] sel;
+wire [7:0] y;
+integer l; 
+demux_1_8 uut(i,sel,y);
+initial begin
+$display("i  |sel  | y7 y6 y5 y4 y3 y2 y1 y0 ");
+$monitor("%b | %b  | %b",i,sel,y);
+i=0;
+for(l=0;l<8;l=l+1) begin 
+ sel=l;#10;
+ end
+i=1;
+for(l=0;l<8;l=l+1) begin
+ sel=l;#10;
+ end
+$finish;
+end
+endmodule
 
